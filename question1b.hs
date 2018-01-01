e8676d0d90d1849b9c712dc742d3f4eeb22eaaa4
tcaron <thomas.caron70@gmail.com>
2018-01-01 20:03:40 +0100

    changement

--- question1b.hs
+++ question1b.hs
@@ -1,8 +1,36 @@
 import System.IO
 
+
+data Shape = Rect Int Int Int Int | 
+    Circle Int Int Int |
+    Line [(Int,Int)] |
+    HashLine [(Int,Int)] deriving (Show)
+
+data Image = Image { 
+    width :: Int,
+    height :: Int,
+    shapes :: [Shape]
+    } deriving(Show)
+ 
+
 begin = "<svg xmlns='http://www.w3.org/2000/svg' width='500' height='500' version='1.1'>"
 end = "</svg>"
 file = "image.svg"
-main = do
-  writeFile file begin
-  appendFile file end
+
+getWidth :: Image -> Int 
+getWidth (Image w _ _) = w 
+
+getHeight :: Image -> Int
+getHeight (Image _ h _) = h
+
+getShapes :: Image -> Int
+getShapes (Image _ _ (h:t)) = h 
+  
+
+export :: Image -> String
+export (Image w h _) = "Veuillez enregistrer des formes à dessiner"
+--export (w h [(l)]) = 
+  -- let img = Image ()
+  --readFile file 
+  --writeFile file begin
+  --appendFile file end
