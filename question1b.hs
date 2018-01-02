7fa606c62c7c3d645abd66d735b7a906db767573
tcaron <thomas.caron70@gmail.com>
2018-01-02 21:56:17 +0100

    export

--- question1b.hs
+++ question1b.hs
@@ -3,33 +3,48 @@ import Text.Regex
 import System.Random
 import Data.Ord
 
-type Point     = (Float,Float)
-type Circle    = (Int,Int,Int)
-type Rectangle = (Int,Int,Int,Int)
-type Triangle  = [Point] 
-type Color     = (Int,Int,Int)
-data Shape     =  Circle | Rectangle | Link | BrokenLink | Polygon deriving(Show)
+type Color     = (Int,Int,Int) 
+data Shape     =  Circle Int Int Int | Rectangle Int Int Int Int | Link Int Int Int Int | BrokenLink Int Int Int Int | Polygon [(Float,Float)]   deriving (Show)
 data DrawingArea = Int Int [Shape]
-type BrokenLink = [Point]
-type Image     = (Int,Int,[Shape])
-type Polygon   = [Point] -- carre / triangle
-type Link      = [Point]
+
+printShape :: Shape -> String 
+printShape s = ""++(show s)++""
+
+colorize :: Color -> [Shape] -> [(Color,Shape)] 
+colorize = zip.repeat
+
+rainbow@[red,green,blue,yellow,purple,teal] = map colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
+
+writePoints :: (Float,Float) -> String 
+writePoints (x,y)= (show x)++","++(show y)++" "
+
+writeType :: (Color,Shape) -> String 
+writeType ((r,g,b),(Circle x y z )) = "<circle cx=\""++(show x)++"\" cy=\""++(show y)++"\" r=\""++(show z)++"\"fill:rgb("++(show r)++","++(show g)++","++(show b)++")\"/>" 
+writeType ((r,g,b),(Rectangle x y z w)) = "<rect x=\""++(show x)++"\" y=\""++(show y)++"\" width=\""++(show z)++"\" height=\""++(show w)++"\" fill:rgb("++(show r)++","++(show g)++","++(show b)++")\"/>" 
+writeType ((r,g,b),(Link x y z w)) = "<line x1=\""++(show x)++"\" y1=\""++(show y)++"\" x2=\""++(show z)++"\" y2=\""++(show w)++"\" stroke:rgb("++(show r)++","++(show g)++","++(show b)++")\"/>" 
+writeType ((r,g,b),(BrokenLink x y z w)) = "<line stroke-dasharray= '5,5' x1=\""++(show x)++"\" y1=\""++(show y)++"\" x2=\""++(show z)++"\" y2=\""++(show w)++"\" stroke:rgb("++(show r)++","++(show g)++","++(show b)++")\"/>" 
+writeType ((r,g,b),(Polygon (h:t))) = "<polygon points=\""++(writePoints h)++"\" style=\"fill:rgb("++(show r)++","++(show g)++","++(show b)++");stroke:black;stroke-width:2\"/>"
+
+writeImg :: [(Color,Shape)] -> String
+writeImg s = "<svg xmlns=\"http://www.w3.org/2000/svg\">"++(concatMap writeType s)++"</svg>"
 
 
 main = do 
-  
-  let writePoint :: Point -> String 
-      writePoint (x,y) = (show x)++","++(show y)++" "
+ 
+ 
+  print $ writeImg (blue[(BrokenLink 15 12 13 15)])
+  print $ printShape (Circle 14 15 20)
 
-  let writeShape :: (Color,Polygon) -> String 
-      writeShape ((r,g,b),p) = "<polygon points=\""++(concatMap writePoint p)++"\" style=\"fill:rgb("++(show r)++","++(show g)++","++(show b)++");stroke:black;stroke-width:2\"/>"
-  
-  let writeShapes :: [(Color,Polygon)] -> String 
-      writeShapes p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"++(concatMap writeShape p)++"</svg>"
-  
-  let colorize :: Color -> [Polygon] -> [(Color,Polygon)] 
-      colorize = zip.repeat
 
-  let rainbow@[red,green,blue,yellow,purple,teal] = map colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
+  
 
-  writeFile "img.svg" $ writeShapes (purple [[(100,100),(200,100),(200,200),(100,200)],[(200,200),(300,200),(300,300),(200,300)]])
+--    let writePoint :: Point -> String 
+--      writePoint (x y) = (show x)++","++(show y)++" "
+
+--  let writeShape :: (Color,Polygon) -> String 
+--     writeShape ((r,g,b),p)  = "<polygon points=\""++(concatMap writePoint p)++"\" style=\"fill:rgb("++(show r)++","++(show g)++","++(show b)++");stroke:black;stroke-width:2\"/>"
+  
+--  let writeShapes :: [(Color,Polygon)] -> String 
+--     writeShapes p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"++(concatMap writeShape p)++"</svg>"
+--  
+--writeFile "img.svg" $ writeShapes (purple [[(100 100) (200 100) (200 200) (100 200)] [(200 200) (300 200) (300 300) (200 300)]])
